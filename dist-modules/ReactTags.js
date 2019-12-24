9272eb1bf5707ddcc63759f9ae3745926f86e43d
Joao Medrado <jcarloz@gmail.com>
2019-12-24 11:06:21 +1100

    Allow children element between input and tags for customization

--- dist-modules/ReactTags.js
+++ dist-modules/ReactTags.js
@@ -377,8 +377,9 @@ var ReactTags = function (_Component) {
         _react2.default.createElement(
           "div",
           { className: this.state.classNames.selected },
-          tagItems,
-          this.props.inline && tagInput
+          this.props.inline && tagInput,
+          this.props.children,
+          tagItems
         ),
         !this.props.inline && tagInput
       );
